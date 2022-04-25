3a750535f6f897247a445638a46c4390046ed30a
iiamalex <alexandermehks@gmail.com>
2022-04-25 15:03:56 +0200

    Comment

--- imports/fetch_keys.py
+++ imports/fetch_keys.py
@@ -1,8 +1,16 @@
-#Can be optimized very much, key.keys() generates a dict that can be returned directly.
+"""
+Can be optimized very much, key.keys() generates a dict that can be returned directly.
+
+Idea: instead of iterating over all the keys check the length of the dict to be able to
+catch if a key has been added for a specific json object. 
+
+len(key.keys()) => check for same length != iterate over the specific dict for new keyy
+"""
 def keys(data):
     key_holder = [] 
     for key in data:
         for k in key.keys():
             if k not in key_holder:
                 key_holder.append(k)
+
     return key_holder
