a165c6589b5c0ca84a1827e9f3d413e6cade4329
iiamalex <alexandermehks@gmail.com>
2022-04-25 14:53:35 +0200

    Update

--- imports/fetch_keys.py
+++ imports/fetch_keys.py
@@ -1,8 +1,8 @@
 #Can be optimized very much, key.keys() generates a dict that can be returned directly.
 def keys(data):
-	key_holder = [] 
-	for key in data:
-		for k in key.keys():
-			if k not in key_holder:
-				key_holder.append(k)
-	return key_holder
+    key_holder = [] 
+    for key in data:
+        for k in key.keys():
+            if k not in key_holder:
+                key_holder.append(k)
+    return key_holder
