6d951251e26aa933e6b790836abb74ed2e24a480
iiamalex <alexandermehks@gmail.com>
2022-04-30 18:53:34 +0200

    Update

--- build.py
+++ build.py
@@ -3,32 +3,60 @@ import requests
 import json
 from imports import compare,haschanged, fetch_keys
 from log import do_log
+import sys
 
-def run():
-    try:
-        #Test with real request on example json.
-        r = requests.get('https://jsonplaceholder.typicode.com/posts')
-        #r = requests.get('https://gorest.co.in/public/v2/posts/100/comments')
 
-        change = compare.do_compare(r.json())
+#Test with real request on example json.
+r = requests.get('https://jsonplaceholder.typicode.com/posts')
+#r = requests.get('https://gorest.co.in/public/v2/posts/100/comments')
 
-        if haschanged.has_changed(change):
-            """
-#            ADD ALERT => Email, sms, slack ? 
-            """
-            do_log(change)
-            with open("latestResponse.json", 'w') as file:
-                file.write(json.dumps(r.json(), indent = 4))
-        else:
-            print("No change in the response")
-            do_log("No change in the response")
+"""
+The search takes an argument of an incoming response from the API you are using.
+The old response gets stored locally to match against the new one for changes.
 
-    except:
-        print("Something went wrong")
+ """
+
+def do_compare(json_data):
+    """
+    do_compare function, compares the new response with the old response stored in
+    latestResponse.json
+
+    :param: Any kind of json data.
+    :return: A dict with added and removed keys. In the case the response has not changed
+    it returns an empty dict
+    
+    """
+
+    PATH_JSON = sys.path[0] + "/latestResponse.json"
+
+    #Latest response from the API. Only looking at the keys. 
+    latest_response_keys = fetch_keys.keys(json_data)
+    
+    #Stored API response.
+    with open(PATH_JSON, "r") as file:
+        data = json.loads(file.read())
+        old_response_keys = fetch_keys.keys(data)
+        file.close()
+
+    #If something has been removed from the response.
+    
+
+    changed_keys = compare.key_compare(latest_response_keys,old_response_keys)
+
+    with open("latestResponse.json", 'w') as file:
+        file.write(json.dumps(r.json(), indent = 4))
+
+    #return change_dict
+    return changed_keys 
+
 
 
 
 if __name__ == "__main__":
-   run() 
-
+    """
+    Runs the do_compare function that compares the old response with the new and
+    looking for changes. 
+    Ret
+    """
+    if haschanged.has_changed(do_compare(r.json())):
 
--- imports/compare.py
+++ imports/compare.py
@@ -1,18 +1,23 @@
-from imports import compare,fetch_keys
+from imports import fetch_keys
 import json
 import os
 import sys
 
-PATH_JSON = sys.path[0] + "/latestResponse.json"
 
-def key_compare(latest_response, old_response, removed):
+def key_compare(latest_response, old_response):
     change_dict = {}
     change_dict["removed_keys"] = []
     change_dict["added_keys"] = []
 
+    #If the length of the new and old response are not the same.
+    if len(latest_response) != len(old_response):
+        modified = True
+    else:
+        modified = False
+
     #Iterating over the latest response, looking at keys in the response.
     for key in latest_response:
-        if removed:
+        if modified:
             for old_key in old_response:
                 if old_key not in latest_response and old_key not in change_dict["removed_keys"]:
                     change_dict["removed_keys"].append(old_key)
@@ -21,29 +26,4 @@ def key_compare(latest_response, old_response, removed):
             change_dict["added_keys"].append(key)
     return change_dict
 
-"""
-The search takes an argument of an incoming response from the API you are using.
-The old response gets stored locally to match against the new one for changes.
 
- """
-def do_compare(json_data):
-    #Latest response from the API. Only looking at the keys. 
-    latest_response_keys = fetch_keys.keys(json_data)
-
-    #Stored API response.
-    with open(PATH_JSON, "r") as file:
-        data = json.loads(file.read())
-        old_response_keys = fetch_keys.keys(data)
-        file.close()
-
-    #If something has been removed from the response.
-    if len(latest_response_keys) != len(old_response_keys):
-        keys_removed = True
-    else:
-        keys_removed = False
-
-    key_compare = compare.key_compare(latest_response_keys,old_response_keys, keys_removed)
-    change_dict = key_compare
-
-    #return change_dict
-    return key_compare
